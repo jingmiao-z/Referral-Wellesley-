d834a3b5fa5d428aef291b66078cd17a819c7ff1
AnnieL66 <yaxinliu.annie@gmail.com>
2021-11-15 10:07:08 -0500

    draft

--- app.py
+++ app.py
@@ -1,26 +1,11 @@
-from flask import (Flask, url_for, render_template, request, redirect, flash)
-# import referral_db as dbi
-import random 
+
+from flask import (Flask, url_for, render_template, request, redirect)
+
 app = Flask(__name__)
 
-app.secret_key = 'your secret here'
-# replace that with a random key
-app.secret_key = ''.join([ random.choice(('ABCDEFGHIJKLMNOPQRSTUVXYZ' +
-                                          'abcdefghijklmnopqrstuvxyz' +
-                                          '0123456789'))
-                           for i in range(20) ])
-
-#student search bar 
-@app.route('/search')
-def search():
-    # conn = dbi.connect()
-    return render_template('search_bar.html')
-
-
-# @app.before_first_request
-# def startup():
-#     dbi.cache_cnf()
-#     dbi.use('referral_db')
+@app.route('/')
+def index():
+    return render_template('mainSearch.html')
 
 if __name__ == '__main__':
     import os
