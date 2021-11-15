23f05d3576a13407449b54934f7f7b574a0f9c31
jingmiao-z <jz1@wellesley.edu>
2021-11-15 17:56:31 -0500

    all front-end-tested without all css

--- app.py
+++ app.py
@@ -5,11 +5,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('baseRegForm.html')
+
+@app.route('/main_search')
+def main_search():
     return render_template('mainSearch.html')
 
-@app.route('/announcement')
-def announcement():
-    return render_template('welcome.html')
+@app.route('/welcome_student')
+def welcome_student():
+    return render_template('welcome_student.html')
+
+@app.route('/welcome_referrer')
+def welcome_referrer():
+    return render_template('welcome_referrer.html')
+
+@app.route('/profile_referrer')
+def profile_referrer():
+    return render_template('profile_referrer.html')
+
+@app.route('/profile_student')
+def profile_student():
+    return render_template('profile_student.html')
+
+
+@app.route('/welcome_admin')
+def welcome_admin():
+    return render_template('welcome_admin.html')
 
 if __name__ == '__main__':
     import os
